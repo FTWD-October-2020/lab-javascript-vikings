396db6827408c8b91a6cfa8b9df053bb3382a297
Viviana Garcia <vivianandrea4@gmail.com>
2020-11-03 15:52:57 -0500

    vivis commit

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,27 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        console.log("AHH")
+        return this.strength;
+    }
+    receiveDamage(damage) {
+        console.log("UFF")
+        this.health -= damage;
+    }
+}
+console.log("rambo")
+let rambo = new Soldier(100, 200)
+rambo.receiveDamage(50)
 
 // Viking
-class Viking {}
+class Viking { }
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
