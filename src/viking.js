61517b58a748acd97cb3c445b7466e3724a52241
Steven Sarria <ssarr012@fiu.edu>
2020-11-03 16:10:14 -0500

    stevens iteration

--- src/viking.js
+++ src/viking.js
@@ -18,7 +18,27 @@ let rambo = new Soldier(100, 200)
 rambo.receiveDamage(50)
 
 // Viking
-class Viking { }
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength)
+        this.name = name;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`
+        } else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return "Odin Owns You All!"
+    }
+}
+let thor = new Viking("THOR", 200, 300)
+rambo.receiveDamage(thor.attack())
+console.log(thor.battleCry())
 
 // Saxon
 class Saxon { }
